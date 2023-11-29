47bf7482474073512bd2cdd550ec654dc5c0f81c
sunkang <13127955213@qq.com>
2023-11-29 17:35:48 +0800

    还原Db

--- lib/Db.php
+++ lib/Db.php
@@ -1,12 +1,12 @@
-<?php
+<?php 
 namespace ExpressTemplate;
 //德邦
-class Db extends Base{
-	public $name = 'db';
+class Db extends Base{  
+	public $name = 'db';  
     public function output($option = []){
-        $this->str = "";
-        $order_no = $option['order_no'];
-        $return_content = $option['return_content'];
+        $this->str = "";  
+        $type = $option['type'];
+        $return_content = $option['return_content']; 
         $name = $option['name'];
         $receiver = $option['receiver'];
         $sender = $option['sender'];
@@ -17,335 +17,302 @@ class Db extends Base{
         $mark = $option['mark'];
         $bag_addr_1 = $option['bag_addr_1'];
         $bag_addr_2 = $option['bag_addr_2'];
-        $insured_amount = $option['insured_amount'];
-        $receipt = $option['receipt']?:'无';
-        $receipt_requirements = $option['receipt_requirements']?:'无';
-        $bill_note = $option['bill_note'];
-        $save_path = $option['save_path'];
-        $payment_method = $option['payment_method'];
-        $transport_type = $option['transport_type'];
-
+        $desc1 = $option['desc1'];
+        $desc2 = $option['desc2']; 
+        $desc = $option['desc'];
+        $save_path = $option['save_path'];  
+        $tip1 = $option['tip1'];  
+        $tip2 = $option['tip2'];  
+        $notice = $option['notice'];   
+        $type_html = "<div style='color:#FFF;background:#000;padding: 6px 8px 6px 13px;'>".($type?:"普快")."</div>";
+        
         $this->bill_code_lr([
             'bill_code'=>$bill_code,
             'top'=>28,
             'space'=>5,
-            'left'=>0.5,
+            'left'=>1,
             'rotate'=>90,
-            'num'=>2,
-            'prefix'=>'',
-            'size'=>'7pt'
-        ]);
-
+            'num'=>2,  
+            'prefix'=>''
+        ]); 
         $this->bill_code_lr([
             'bill_code'=>$bill_code,
             'top'=>21,
             'space'=>5,
-            'right'=>0.5,
+            'right'=>1,
             'rotate'=>-90,
-            'num'=>2,
-            'prefix'=>'',
-            'size'=>'7pt'
-        ]);
+            'num'=>3, 
+            'prefix'=>''
+        ]); 
 
-        $this->text([
-            'top'=>1,
-            'left'=>2,
-            'text'=>'德邦快递',
-            'size'=>'18pt',
-        ]);
-
-        $this->text([
-            'top'=>10,
-            'left'=>0.5,
+        $this->text([   
+            'top'=>10, 
+            'left'=>1,
             'text'=>'实名验证',
-            'size'=>'7pt',
+            'size'=>'8px',
             'rotate'=>90,
-        ]);
+        ]); 
 
-        $this->text([
-            'top'=>10,
-            'right'=>0.5,
+        $this->text([   
+            'top'=>10, 
+            'right'=>1,
             'text'=>'已验视',
-            'size'=>'7pt',
+            'size'=>'8px',
             'rotate'=>-90,
-        ]);
+        ]); 
 
-        $this->text([
-            'top'=>1,
-            'right'=>4,
-            'text'=>$transport_type,
-            'width'=>6,
-            'size'=>'8pt',
+        $this->text([   
+            'top'=>3, 
+            'right'=>5,
+            'text'=>$notice, 
+            'width'=>9,
+            'size'=>'13px',
             'rotate'=>0,
-        ]);
+        ]); 
 
-        $this->text([
-            'top'=>105,
-            'right'=>0.5,
+        $this->text([   
+            'top'=>105, 
+            'right'=>1,
             'text'=>'https://deppon.com',
-            'size'=>'7pt',
+            'size'=>'8px',
             'rotate'=>-90,
-        ]);
+        ]); 
+         
 
-        $this->text([
-            'top'=>100,
+        $this->text([   
+            'top'=>100, 
             'left'=>1,
             'text'=>$option['time']?:now(),
-            'size'=>'7pt',
+            'size'=>'8px',
             'rotate'=>90,
-        ]);
+        ]); 
 
-        $this->text([
-            'top'=>10,
-            'left' => 5,
-            'right' => 5,
-            'text'=>$mark,
-            'size'=>'20pt',
-        ]);
+        $this->text([   
+            'top'=>8, 
+            'left'=>4,
+            'right'=>4,
+            'text'=>$this->center($mark,'16px'),
+            'size'=>'16px',
+            'bold'=>true,
+        ]); 
+        $this->text([ 
+            'top'=>16, 
+            'left'=>4, 
+            'size'=>'9px',
+            'right'=>22,
+            'text'=>"[送]",
+            'bold'=>false,
+        ]); 
+        $this->text([ 
+            'top'=>16, 
+            'left'=>9, 
+            'size'=>'9px',
+            'right'=>22,
+            'text'=>$option['to']?:$this->parse_address($receiver['address']),
+            'bold'=>false,
+        ]);  
 
-        $this->text([
-            'top'=>26,
-            'left' => 5,
-            'size'=>'8pt',
+        $this->text([ 
+            'top'=>26, 
+            'left'=>4, 
+            'size'=>'9px',
             'width'=>46,
             'text'=>$title,
-        ]);
+            'bold'=>false,
+        ]); 
 
-        $this->text([
-            'top'=>26,
-            'left' => 5,
-            'right' => 5,
-            'text'=>$this->right($sub_title, '8pt'),
-        ]);
-
-        if($sub_bill_code){
-            $bill_code_text = substr($bill_code, 0, 3).'&nbsp;'.substr($bill_code, 3, 4).'&nbsp;'.substr($bill_code, 7, 4).'&nbsp;'.substr($bill_code, 11);
+        $this->text([ 
+            'top'=>26, 
+            'left'=>54, 
+            'size'=>'9px', 
+            'width'=>15, 
+            'text'=>$sub_title,
+            'bold'=>false,
+        ]);  
+        if($sub_bill_code){ 
             if($bill_code != $sub_bill_code){
-                $this->text([
-                    'left'=>5,
-                    'right'=>5,
-                    'top'=>31,
-                    'text'=>$this->right("[母]&nbsp;".$bill_code_text, '14pt'),
-                ]);
-                $this->text([
-                    'left'=>5,
-                    'right'=>5,
-                    'top'=>47.5,
-                    'text'=>$this->right("[子]&nbsp;".$bill_code_text, '8pt'),
-                ]);
-                $this->text([
-                    'left'=>5,
-                    'right'=>5,
-                    'top'=>37,
-                    'text'=>$this->barcode($sub_bill_code,['height'=>40]),
-                ]);
+                $this->text([ 
+                    'left'=>5, 
+                    'right'=>5, 
+                    'top'=>31, 
+                    'text'=>$this->center("母：".$bill_code,12),
+                ]);  
+                $this->text([ 
+                    'left'=>5, 
+                    'right'=>5, 
+                    'top'=>34, 
+                    'text'=>$this->barcode($sub_bill_code,['height'=>40,'prefix'=>'子：']),
+                ]);  
             }else{
-                $this->text([
-                    'left'=>5,
-                    'right' => 5,
-                    'top'=>32,
-                    'text'=>$this->right('[母]&nbsp;'.$bill_code_text, '14pt')
-                ]);
-                $this->text([
-                    'left'=>5,
-                    'right'=>5,
-                    'top'=>38,
-                    'text'=>$this->barcode($sub_bill_code,['height'=>40]),
-                ]);
+                $this->text([ 
+                    'left'=>5, 
+                    'right'=>5, 
+                    'top'=>31, 
+                    'text'=>$this->barcode($sub_bill_code,['height'=>50,'prefix'=>'母：']),
+                ]);  
             }
-
+            
         }else{
-            $bill_code_text = substr($bill_code, 0, 3).'&nbsp;'.substr($bill_code, 3, 4).'&nbsp;'.substr($bill_code, 7, 4).'&nbsp;'.substr($bill_code, 11);
-            $this->text([
-                'left'=>5,
-                'right' => 5,
-                'top'=>32,
-                'text'=>$this->right($bill_code_text, '14pt')
-            ]);
-            $this->text([
-                'left' => 5,
-                'right' => 5,
-                'top'=>38,
+            $this->text([ 
+                'left'=>5, 
+                'right'=>5, 
+                'top'=>31, 
                 'text'=>$this->barcode($bill_code,['height'=>50]),
-            ]);
+            ]);  
         }
+        
 
-        $this->line([
-            'top'=>31,
-            'left' => 5,
-            'width' => 66
-        ]);
+        $this->line([ 
+            'top'=>31, 
+            'left'=>4, 
+        ]); 
 
-        $this->line([
-            'top'=>52,
-            'left' => 5,
-            'width' => 66
-        ]);
+        $this->line([ 
+            'top'=>52, 
+            'left'=>4, 
+        ]); 
+ 
+        $this->text([ 
+            'top'=>53, 
+            'left'=>4, 
+            'size'=>'12px',
+            'text'=>"收:&nbsp;&nbsp;".$this->parse_name($receiver['name']) ."&nbsp;&nbsp;&nbsp;&nbsp;".$this->parse_phone($receiver['phone']),
+            'bold'=>false,
+        ]); 
 
-        $this->text([
-            'top'=>53,
-            'left' => 5,
-            'size'=>'8pt',
-            'text'=>"收：".$this->parse_name($receiver['name']) ."&emsp;&emsp;&emsp;&emsp;".$receiver['company'],
-        ]);
-
-        $this->text([
-            'top'=>56,
-            'left' => 5,
-            'size'=>'8pt',
-            'text'=>$this->parse_phone($receiver['phone']),
-        ]);
-
-        $this->text([
-            'top'=>59,
-            'left' => 5,
-            'size'=>'8pt',
-            'right' => 5,
+        $this->text([ 
+            'top'=>58, 
+            'left'=>4, 
+            'size'=>'9px',
+            'right'=>22,
             'text'=>$this->parse_address($receiver['address']),
-        ]);
+            'bold'=>false,
+        ]); 
 
-        $this->line([
-            'top'=>66,
-            'left' => 5,
-            'width' => 66
-        ]);
+        $this->line([ 
+            'top'=>66, 
+            'left'=>4, 
+        ]); 
 
-        $this->text([
-            'top'=>67,
-            'left'=>13,
-            'right' => 5,
-            'size'=>'8pt',
-            'text'=>"寄：".$this->parse_name($sender['name']),
-        ]);
+        $this->text([ 
+            'top'=>68, 
+            'left'=>17, 
+            'size'=>'9px',
+            'text'=>"寄: &nbsp;&nbsp;".$this->parse_name($sender['name']) ."&nbsp;&nbsp;&nbsp;&nbsp;".$this->parse_phone($sender['phone']), 
+        ]); 
+        $this->text([ 
+            'top'=>72, 
+            'left'=>17, 
+            'right'=>4, 
+            'size'=>'7px',
+            'text'=>$this->parse_address($sender['address']), 
+        ]); 
+        
+        $this->line([ 
+            'top'=>78, 
+            'left'=>17, 
+            'width'=>55, 
+        ]); 
 
-        $this->text([
-            'top'=>67,
-            'left'=>13,
-            'right' => 5,
-            'text'=>$this->right($sender['company'], '8pt'),
-        ]);
-
-        $this->text([
-            'top'=>72,
-            'left'=>13,
-            'right' => 5,
-            'size'=>'8pt',
-            'text'=>$this->parse_phone($sender['phone']),
-        ]);
-
-        $this->line([
-            'top'=>77,
-            'left'=>13,
-            'width'=>58,
-        ]);
-
-        $this->text([
-            'top'=>78,
-            'left'=>13,
-            'size'=>'8pt',
-            'right' => 5,
+        $this->text([ 
+            'top'=>79, 
+            'left'=>17, 
+            'size'=>'8px',
+            'right'=>4,
             'text'=>"品名：".$name,
-        ]);
+            'bold'=>false,
+        ]); 
 
-        $this->text([
-            'top'=>87,
-            'left'=>13,
-            'size'=>'8pt',
-            'right' => 5,
-            'text'=>"备注：".$bill_note,
-        ]);
+        $this->text([ 
+            'top'=>87, 
+            'left'=>17, 
+            'size'=>'8px',
+            'right'=>4,
+            'text'=>"备注：".$desc,
+            'bold'=>false,
+        ]); 
 
-        $this->text([
-            'left'=>3,
+        $this->text([ 
+            'left'=>3, 
             'top'=>68,
             'text'=>$this->barcode($bill_code,['width'=>55,'height'=>40,'no_text'=>true]),
             'rotate'=>-90,
-        ]);
+        ]);  
 
-        $this->line([
-            'top'=>98,
-            'left'=>13,
-            'width'=>58,
-        ]);
+        $this->line([ 
+            'top'=>97, 
+            'left'=>17, 
+            'width'=>55, 
+        ]); 
 
-        $this->line([
-            'top'=>112,
-            'left'=>13,
-            'width'=>59,
-        ]);
+        $this->line([ 
+            'top'=>110, 
+            'left'=>17, 
+            'width'=>55, 
+        ]); 
 
-        $this->text([
-            'left'=>13,
+        $this->text([ 
+            'left'=>17, 
             'top'=>98,
             'right'=>10,
-            'text'=>'签收单：'.$receipt,
-            'size'=>'8pt',
+            'text'=>$desc1, 
+            'size'=>'8px',
         ]);
 
-        $this->text([
-            'left'=>13,
-            'top'=>102,
+        $this->text([ 
+            'left'=>17, 
+            'top'=>111,
             'right'=>10,
-            'text'=>'返单要求：'.$receipt_requirements,
-            'size'=>'8pt',
+            'text'=>$desc2, 
+            'size'=>'8px',
+        ]);    
+
+        $this->line([ 
+            'top'=>113, 
+            'right'=>4, 
+            'width'=>22, 
         ]);
 
-        $this->line([
-            'top'=>113,
-            'right' => 5,
+        $this->line([ 
+            'top'=>118, 
+            'right'=>4, 
+            'width'=>22, 
+        ]);  
+
+        $this->line([ 
+            'top'=>126, 
+            'right'=>4, 
+            'width'=>22, 
+        ]); 
+
+        $this->vline([ 
+            'top'=>113, 
+            'height'=>13.3, 
+            'right'=>26, 
+        ]);
+        $this->vline([ 
+            'top'=>113, 
+            'height'=>13.3, 
+            'right'=>4, 
+        ]);
+
+        $this->text([  
+            'top'=>114,
+            'right'=>4,
             'width'=>22,
-        ]);
+            'text'=>$this->center($tip1,8), 
+            'size'=>'8px',
+        ]); 
 
-        $this->line([
-            'top'=>118,
-            'right' => 5,
-            'width'=>22,
-        ]);
-
-        $this->line([
-            'top'=>126,
-            'right' => 5,
-            'width'=>22,
-        ]);
-
-        $this->vline([
-            'top'=>113,
-            'height'=>13.3,
-            'right'=>27,
-        ]);
-        $this->vline([
-            'top'=>113,
-            'height'=>13.3,
-            'right' => 5,
-        ]);
-
-        if ($insured_amount) {
-            $this->text([
-                'left'=>13,
-                'top'=>113,
-                'right'=>10,
-                'text'=>'保价金额：'.$insured_amount,
-                'size'=>'8pt',
-            ]);
-        }
-
-        $this->text([
-            'left'=>13,
-            'top'=>118,
-            'right'=>23,
-            'text'=>'用户单号：'.substr($order_no, 18),
-            'size'=>'8pt',
-        ]);
-
-        $this->text([
+        $this->text([  
             'top'=>119,
-            'right' => 5,
+            'right'=>4,
             'width'=>21,
-            'text'=>$payment_method,
-            'size'=>'8pt',
-        ]);
-
-        $body  = $this->render();
-        return $this->do_output($body,$save_path,$return_content);
+            'text'=>$tip2, 
+            'size'=>'8px',
+        ]);     
+     
+        $body  = $this->render();   
+        return $this->do_output($body,$save_path,$return_content); 
     }
 }
